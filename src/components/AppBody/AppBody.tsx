88525b9385bb25816c10957801c379dbc7b17eca
ryougifujino <ryougifujino@gmail.com>
2020-05-05 23:58:14 +0800

    Add AppBody

---
+++ src/components/AppBody/AppBody.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './AppBody.module.less';
+
+const AppBody: React.FunctionComponent = () => <div className={styles.body}>AppBody</div>;
+
+export default AppBody;
--- src/screens/App/App.tsx
+++ src/screens/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.less';
 import AppHeader from '../../components/AppHeader/AppHeader';
+import AppBody from '../../components/AppBody/AppBody';
 
 const App: React.FunctionComponent = () => (
   <div>
     <AppHeader />
+    <AppBody />
   </div>
 );
 
