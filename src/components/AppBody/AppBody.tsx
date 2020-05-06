9531081e84e882e66ff7f62f3a417dee62c7098f
ryougifujino <ryougifujino@gmail.com>
2020-05-06 23:46:47 +0800

    Add basic Photo

--- src/components/AppBody/AppBody.tsx
+++ src/components/AppBody/AppBody.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import styles from './AppBody.module.less';
+import Photo from '../UI/Photo/Photo';
 
-const AppBody: React.FunctionComponent = () => <div className={styles.body}>AppBody</div>;
+const AppBody: React.FunctionComponent = () => (
+  <div className={styles.body}>
+    {Array(10)
+      .fill(null)
+      .map(() => (
+        <Photo />
+      ))}
+  </div>
+);
 
 export default AppBody;
---
+++ src/components/UI/Photo/Photo.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Photo.module.less';
+
+const Photo: React.FunctionComponent = () => <div className={styles.photo}></div>;
+
+export default Photo;
